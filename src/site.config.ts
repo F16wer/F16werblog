0f6cec5c64158273d6058ff5fd079d4cf11ba7db
F16wer <2537443928@qq.com>
2026-02-19 14:48:42 +0800

    把评论区服务器迁移至国内

--- src/site.config.ts
+++ src/site.config.ts
@@ -161,7 +161,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://1234-five-beige.vercel.app/',
+    server: 'https://comments.f16wer.com',
     // Show meta info for comments
     showMeta: false,
     // Refer https://waline.js.org/en/guide/features/emoji.html
