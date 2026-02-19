d7115b5fd6dea6747bb11206c17b924f48ba3f46
F16wer <2537443928@qq.com>
2026-02-19 14:23:24 +0800

    换了个评论区配置

--- src/site.config.ts
+++ src/site.config.ts
@@ -161,7 +161,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: 'https://123-sable-delta.vercel.app/',
+    server: 'https://1234-five-beige.vercel.app/',
     // Show meta info for comments
     showMeta: false,
     // Refer https://waline.js.org/en/guide/features/emoji.html
